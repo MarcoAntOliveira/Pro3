6e69e703e55f1a798dd7225c984ab4da2bbdec29
marcoolivera101 <111010339+MarcoAntOliveira@users.noreply.github.com>
2022-10-31 15:41:51 -0300

    Create Empresa.h

---
+++ Empresa.h
@@ -0,0 +1,18 @@
+#ifndef RETANGULO_H
+#define RETANGULO_H
+#include <iostream>
+#include "Departamento.h"
+#include <vector>
+
+
+class empresa {
+std::string nome;
+int CNPJ;
+int qtd_departamentos = 10;
+std::vector <departamento>departamento1();
+
+public:
+empresa(std::string _nome, int _CNPJ);
+
+};
+#endif
