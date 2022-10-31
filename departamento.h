2d4e3ec3dd8e77f27c856ee1ad0be9fb51df55bd
marcoolivera101 <111010339+MarcoAntOliveira@users.noreply.github.com>
2022-10-31 15:40:35 -0300

    Create departamento.h

---
+++ departamento.h
@@ -0,0 +1,8 @@
+#include "Empresa/Departamento.h"
+#include "Empresa/funcionarios.h"
+
+#include <iostream>
+
+departamento:: departamento(std::string _departamento1){
+    departamento1 = _departamento1;
+}
